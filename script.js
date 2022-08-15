c448234068440cd22f71a88d167034baf831e8e6
ashok <1999ashokkumawat>
2022-08-15 21:43:19 +0530

    first commit

--- script.js
+++ script.js
@@ -1,36 +1,26 @@
-console.log("calculator calculation");
+   console.log("ram ram ji");
+   let string = "";
+   let  buttons = document.querySelectorAll('.button');
+   Array.from(buttons).forEach((btn)=>btn.addEventListener('click',()=>{this
+       this.style.color = "blue";
+   }))
+   Array.from(buttons).forEach((all) => {
+    all.addEventListener('click', (e)=>{
+        if(e.target.innerHTML == "="){
+            string = eval(string);
+            document.getElementById('screen').value = string;
+        }
+        else if(e.target.innerHTML == "c"){
+            string = "" ;
+            document.getElementById('screen').value = string;
+        }
 
-//  let string = "";
-//  buttons = document.querySelectorAll('.button');
-
-//   Array.from(buttons).forEach((button) => {
-//     button.addEventListener('click', (e)=>{
-//         console.log(e.target);
-//     })
-//   });
-const screen = document.getElementById("screen");
-const buttons = document.querySelectorAll("button");
-let screenValue = screen.value;
-
-for (item of buttons) {
-  item.addEventListener("click", (e) => {
-
-    let buttonText = e.target.innerText;
-    // console.log("button Text is", buttonText);
-    if (buttonText == "X") {
-      buttonText = "*";
-      screen.value += screenValue;
-      screenValue = buttonText;
-        }    else if (buttonText == "c") {
-      screenValue = " ";
-      screen.value = screenValue;
-    } else if (buttonText == "=") {
-      screen.value = eval(screenValue);
-    } else {
-      screenValue += buttonText;
-      screen.value = screenValue;
-    }
-  });
+        else{
+       console.log(e.target);
+       string = string + e.target.innerHTML;
+       document.getElementById('screen').value = string;}
+     })
+   }
+  
+   );
      
-
-}
